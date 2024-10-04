d922076a337b6d0a93b4bac0e5a1b602d8054da6
Oliver Daniels-Koch <danielskoch.oliver@gmail.com>
2024-10-04 16:39:44 -0400

    2d mix rate sweep and results plotting

--- toy_2d_mix_rate_sweeps.py
+++ toy_2d_mix_rate_sweeps.py
@@ -3,12 +3,13 @@ import subprocess
 from tqdm import tqdm
 
 # mix rates
-mix_rates = [i * 1e-2 for i in range(1, 10)] + [i * 1e-1 for i in range(1, 11)]
+mix_rates = []
+mix_rates.extend([i * 1e-2 for i in range(1, 10)])
+mix_rates.extend([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
 
 i = 0
 for loss_type in [LossType.PROB, LossType.EXP, LossType.DIVDIS]:
     for mix_rate in tqdm(mix_rates, desc=f'loss_type={loss_type}'):
-        # run python toy_2d_ace_prob_mix_rates.py --loss_type {loss_type} --mix_rate {mix_rate}
         command = [
             'python',
             'toy_2d.py',
