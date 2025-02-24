4d499bdeff051ff4aedaf5832dccfba6e2367d3a
oliveradk <danielskoch.oliver@gmail.com>
2025-02-24 12:31:46 -0800

    update mr_lb script to use partitions

--- experiments/mix_rate_lb_sweep/0_mix_rate_lb_sweep.py
+++ experiments/mix_rate_lb_sweep/0_mix_rate_lb_sweep.py
@@ -1,4 +1,3 @@
-#%%
 import os 
 os.chdir("/nas/ucb/oliveradk/diverse-gen/")
 
@@ -13,12 +12,12 @@ from omegaconf import OmegaConf
 from diverse_gen.losses.loss_types import LossType
 from diverse_gen.utils.exp_utils import get_study_args_dict, get_executor, run_experiments
 from diverse_gen.utils.run_study import get_storage_path
-#%%
+
 N_TRIALS = 200
-N_PARTITIONS = 1 # no longer using multiple partitions
+N_PARTITIONS = 4 # no longer using multiple partitions
 SAMPLER = "grid"
 STUDY_SCRIPT_PATH = "experiments/mix_rate_lb_sweep/run_mr_lb_study.py"
-#%%
+
 SCRIPT_NAME = "exp_scripts/spur_corr_exp.py"
 EXP_DIR = Path("output/mix_rate_lb_sweep")
 SUB_DIR = None
@@ -26,11 +25,19 @@ if SUB_DIR is None:
     SUB_DIR = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 EXP_DIR = Path(EXP_DIR, SUB_DIR)
 EXP_DIR.mkdir(exist_ok=True, parents=True)
-#%%
+
 MIX_RATES = [0.1, 0.25, 0.5, 0.75, 1.0]
-DATASETS = ["toy_grid", "fmnist_mnist", "cifar_mnist", "waterbirds"]
+DATASETS = [
+    "toy_grid", 
+    "fmnist_mnist", 
+    "cifar_mnist", 
+    "waterbirds", 
+    "celebA-0", 
+    "multi-nli"
+]
+
 METHODS = ["TopK_0.5"]
-#%%
+
 HPARAM_MAP = {
     "mix_rate_lower_bound": {"type": "float", "range": [0.0, 1.0], "log": False},
 }
@@ -38,6 +45,23 @@ search_space = {
     "mix_rate_lower_bound": np.linspace(0.0, 1.0, 20).tolist(), 
     "mix_rate_lower_bound_01": np.linspace(0.0, 1.0, 10).tolist(), 
 }
+
+def update_map_and_search_space(hparam_map, search_space, idx):
+    hparam_map = copy.deepcopy(hparam_map)
+    search_space = copy.deepcopy(search_space)
+    k = "mix_rate_lower_bound"
+    v = hparam_map[k]
+    # compute new range
+    inc_size = (v["range"][1] - v["range"][0]) / N_PARTITIONS
+    new_range = [v["range"][0] + idx * inc_size, v["range"][0] + (idx + 1) * inc_size] 
+    
+    # update hparam map
+    hparam_map[k]["range"] = new_range
+    # update search space
+    search_space[k] = np.linspace(new_range[0], new_range[1], 20).tolist()
+
+    return hparam_map, search_space
+
 
 # generate exp configs
 configs = {
@@ -53,12 +77,11 @@ configs = {
 for conf in configs.values():
     conf["aggregate_mix_rate"] = False
 
-#%%
 def get_study_name(ds_name, mix_rate):
     return f"{ds_name}_{mix_rate}"
 
 # run experiments
-for (ds_name, mix_rate), conf in configs.items(): 
+for (ds_name, method_name, mix_rate), conf in configs.items(): 
     study_name = get_study_name(ds_name, mix_rate)
     study_dir = Path(EXP_DIR, study_name)
     study_dir.mkdir(exist_ok=True, parents=True)
@@ -68,12 +91,15 @@ for (ds_name, mix_rate), conf in configs.items():
     
     # run study
     n_trials_per_node = N_TRIALS // N_PARTITIONS
-    cmds = [
-        {
-            **get_study_args_dict(
+    cmds = []
+    for i in range(N_PARTITIONS):
+        hparam_map, search_space = update_map_and_search_space(HPARAM_MAP, search_space, i)
+        cmds.append(
+            {
+                **get_study_args_dict(
                 conf, 
                 SCRIPT_NAME, 
-                HPARAM_MAP, 
+                hparam_map, 
                 n_trials_per_node, 
                 0, 
                 study_name, 
@@ -82,8 +108,8 @@ for (ds_name, mix_rate), conf in configs.items():
             "search_space": search_space, 
             "sampler_seed": i, 
             "sampler_type": SAMPLER
-        } for i in range(N_PARTITIONS)
-    ]
-    executor = get_executor(study_dir, mem_gb=16, slurm_array_parallelism=N_PARTITIONS)
+        })
+    mem_gb = 16 if ds_name not in ["celebA-0", "multi-nli"] else 32
+    executor = get_executor(study_dir, mem_gb=mem_gb, slurm_array_parallelism=N_PARTITIONS)
 
     jobs = run_experiments(executor, cmds, STUDY_SCRIPT_PATH)
