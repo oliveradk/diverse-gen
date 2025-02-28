8672c5fa3e3647ccd90f9372c095f63b4e8aa341
oliveradk <danielskoch.oliver@gmail.com>
2025-02-28 10:39:19 -0800

    upated mtd script using new config system

--- experiments/mtd/1_mtd_exps.py
+++ experiments/mtd/1_mtd_exps.py
@@ -1,44 +1,52 @@
 from pathlib import Path
 from itertools import product
+from datetime import datetime
 
+import numpy as np
 from omegaconf import OmegaConf
 
 from diverse_gen.utils.exp_utils import get_executor, get_conf_dir, run_experiments
 
 SCRIPT_NAME = "exp_scripts/measurement_tampering.py"
-EXP_DIR = "output/mtd_aux_weight_sweep"
-n_trials = 16
+EXP_DIR = Path("output/mtd")
+SUB_DIR = None
+if SUB_DIR is None:
+    SUB_DIR = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+EXP_DIR = Path(EXP_DIR, SUB_DIR)
+EXP_DIR.mkdir(parents=True, exist_ok=True)
 
-config_dir = Path("configs/mtd")
-methods = OmegaConf.load(config_dir / "mtd_methods.yaml")
-datasets = OmegaConf.load(config_dir / "mt_datasets.yaml")
-method_ds = OmegaConf.load(config_dir / "mtd_method_ds.yaml")
+JOBS = 4
+METHODS = [
+    "TopK_0.1", 
+    "DivDis", 
+    "FT_Trusted", 
+    "Probe_for_Evidence_of_Tamper", 
+    "Measurement_Predictor"
+]
+DATASETS = ["diamonds", "generated_stories"]
+SEEDS = [0, 1, 2, 3, 4, 5, 6, 7]
 
 
-# build configs 
-configs = {}
-for (ds_name, ds_config), (method_name, method_config), seed in product(
-    datasets.items(), 
-    methods.items(), 
-    range(n_trials)
-):
-    key = (ds_name, method_name, seed)
-    conf = {
-        **ds_config, 
-        **method_config, 
-        "seed": seed, 
-        **method_ds[method_name].get(ds_name, {})
+# generate exp configs
+configs = {
+    (ds_name, method_name, seed): {
+        "--config_file": f"{method_name}_{ds_name}", 
+        "seed": seed
     } 
-    if ds_name == "diamonds":
-        conf["dataset"] += f"-seed{seed}"
-        conf["model"] += f"-seed{seed}"
-    conf["exp_dir"] = get_conf_dir(key, Path(EXP_DIR))
-    configs[key] = conf
+    for (ds_name, method_name, seed) in 
+    product(DATASETS, METHODS, SEEDS)
+}
+
+# set exp dirs
+for conf_name, conf in configs.items():
+    conf["exp_dir"] = get_conf_dir(conf_name, Path(EXP_DIR))
 
 # run experiments 
 non_80gb_nodes = ["ddpg", "dqn", "gail", "gan","ppo", "vae"]
 slurm_exclude = ",".join([f"{node}.ist.berkeley.edu" for node in non_80gb_nodes])
-executor = get_executor(EXP_DIR, mem_gb=32, slurm_exclude=slurm_exclude)
-jobs = run_experiments(executor, list(configs.values()), SCRIPT_NAME)
+chunks = np.array_split(list(configs.values()), JOBS)
+for i, chunk in enumerate(chunks):
+    executor = get_executor(EXP_DIR, mem_gb=32, slurm_exclude=slurm_exclude)
+    jobs = run_experiments(executor, chunk, SCRIPT_NAME)
 
 
